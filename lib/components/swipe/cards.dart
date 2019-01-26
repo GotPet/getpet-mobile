05541509052cd0487442934e1c65d4e6be6982bb
Karolis Vycius <k.vycius@gmail.com>
2019-01-26 22:13:27 +0200

    Disable clicking on back card

--- lib/components/swipe/cards.dart
+++ lib/components/swipe/cards.dart
@@ -87,15 +87,16 @@ class _CardStackState extends State<CardStack> {
       alignment: Alignment.center,
       child: new PetCard(
         pet: widget.matchEngine.nextMatch.pet,
+        clickable: false,
       ),
     );
   }
 
   Widget _buildFrontCard() {
-    print(widget.matchEngine.currentMatch.pet.id.toString());
     return new PetCard(
       key: _frontCard,
       pet: widget.matchEngine.currentMatch.pet,
+      clickable: true,
     );
   }
 
@@ -365,85 +366,93 @@ class _DraggableCardState extends State<DraggableCard>
 
 class PetCard extends StatelessWidget {
   final Pet pet;
+  final bool clickable;
 
   const PetCard({
     Key key,
     @required this.pet,
+    this.clickable = true,
   })  : assert(pet != null),
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: Card(
-        margin: EdgeInsets.all(8),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Image.network(
-              pet.profilePhoto,
-              fit: BoxFit.cover,
-            ),
-            Positioned(
-              left: 0.0,
-              right: 0.0,
-              bottom: 0.0,
-              child: new Container(
-                decoration: new BoxDecoration(
-                  gradient: new LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Colors.transparent,
-                      Colors.black.withOpacity(0.8),
-                    ],
-                  ),
-                ),
-                padding: const EdgeInsets.all(24.0),
-                child: new Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    new Expanded(
-                      child: new Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          new Text(
-                            pet.name,
-                            style: new TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 26.0,
-                            ),
-                          ),
-                          new Text(
-                            pet.shortDescription,
-                            style: new TextStyle(
-                              color: Colors.white,
-                              fontSize: 18.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    new Icon(
-                      Icons.info,
-                      color: Colors.white,
-                    ),
+    final card = Card(
+      margin: EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.network(
+            pet.profilePhoto,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: new Container(
+              decoration: new BoxDecoration(
+                gradient: new LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.black.withOpacity(0.8),
                   ],
                 ),
               ),
+              padding: const EdgeInsets.all(24.0),
+              child: new Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  new Expanded(
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        new Text(
+                          pet.name,
+                          style: new TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 26.0,
+                          ),
+                        ),
+                        new Text(
+                          pet.shortDescription,
+                          style: new TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  new Icon(
+                    Icons.info,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(builder: (_) => PetProfileComponent(pet: pet)),
           ),
+        ],
+      ),
     );
+
+    if (clickable) {
+      return InkWell(
+        child: card,
+        onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => PetProfileComponent(pet: pet)),
+            ),
+      );
+    }
+
+    return card;
   }
 }
