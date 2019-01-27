360d4116c00991bda495c8606d66cf2b1d307cec
Karolis Vycius <k.vycius@gmail.com>
2019-01-27 15:08:41 +0200

    Sentry for error reporting

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:getpet/components/app/app_component.dart';
+import 'package:getpet/utils/crash_reporting.dart';
+import 'package:getpet/utils/debug_utils.dart';
 
-void main() {
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
+Future<Null> main() async {
+  CrashReporting crashReporting = await CrashReporting().init();
+
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (isInDebugMode) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  runZoned<Future<Null>>(() async {
+    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     runApp(new AppComponent());
+  }, onError: (error, stackTrace) async {
+    await crashReporting.reportError(error, stackTrace);
   });
 }
