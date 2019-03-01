d192b23474051b590ea13f5103384edcb72bf881
Karolis Vycius <k.vycius@gmail.com>
2019-03-01 15:55:46 +0200

    Show message when unable to call or e-mail

--- lib/components/shelter_pet/shelter_pet_component.dart
+++ lib/components/shelter_pet/shelter_pet_component.dart
@@ -17,127 +17,146 @@ class ShelterPetComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     PetsService().shelterPet(pet);
 
-    callShelter() async {
-      var url = 'tel:${pet.shelter.phone}';
-      if (await canLaunch(url)) {
-        await launch(url);
-      } else {
-        Scaffold.of(context).showSnackBar(
-          SnackBar(
-            content: Text(
-              AppLocalizations.of(context).errorUnableToCallShelter,
-            ),
-          ),
-        );
-      }
-    }
-
-    emailShelter() async {
-      var url = 'mailto:${pet.shelter.email}';
-      if (await canLaunch(url)) {
-        await launch(url);
-      } else {
-        Scaffold.of(context).showSnackBar(
-          SnackBar(
-            content: Text(
-              AppLocalizations.of(context).errorUnableToEmailShelter,
-            ),
-          ),
-        );
-      }
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text(pet.name),
       ),
-      body: Container(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                        pet.profilePhoto,
+      body: Builder(
+        builder: (BuildContext context) {
+          return Container(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            pet.profilePhoto,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 8,
-                    bottom: 8,
-                  ),
-                  child: Text(
-                    pet.name,
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 8,
+                        bottom: 8,
+                      ),
+                      child: Text(
+                        pet.name,
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Text(
-                  pet.shortDescription,
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16, bottom: 8),
-                  child: Text(
-                    pet.shelter.name,
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                InkWell(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 8, bottom: 8),
-                    child: Text(
-                      pet.shelter.phone,
+                    Text(
+                      pet.shortDescription,
                       style: TextStyle(
                         fontSize: 16,
-                        decoration: TextDecoration.underline,
-                        color: Colors.blue,
                       ),
                     ),
-                  ),
-                  onTap: callShelter,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16, bottom: 8),
+                      child: Text(
+                        pet.shelter.name,
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8, bottom: 8),
+                        child: Text(
+                          pet.shelter.phone,
+                          style: TextStyle(
+                            fontSize: 16,
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                      onTap: () async {
+                        var url = 'tel:${pet.shelter.phone}';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                AppLocalizations.of(context)
+                                    .errorUnableToCallShelter,
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                    InkWell(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8, bottom: 8),
+                        child: Text(
+                          pet.shelter.email,
+                          style: TextStyle(
+                            fontSize: 16,
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                      onTap: () async {
+                        var url = 'mailto:${pet.shelter.email}';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                AppLocalizations.of(context)
+                                    .errorUnableToEmailShelter,
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                    )
+                  ],
                 ),
-                InkWell(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 8, bottom: 8),
-                    child: Text(
-                      pet.shelter.email,
-                      style: TextStyle(
-                        fontSize: 16,
-                        decoration: TextDecoration.underline,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                  onTap: emailShelter,
-                )
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
-      floatingActionButton: new FloatingActionButton.extended(
-        icon: Icon(Icons.phone_in_talk),
-        label: Text(AppLocalizations.of(context).callShelter),
-        backgroundColor: Theme.of(context).primaryColor,
-        foregroundColor: Colors.white,
-        onPressed: callShelter,
+      floatingActionButton: Builder(
+        builder: (BuildContext context) {
+          return FloatingActionButton.extended(
+            icon: Icon(Icons.phone_in_talk),
+            label: Text(AppLocalizations.of(context).callShelter),
+            backgroundColor: Theme.of(context).primaryColor,
+            foregroundColor: Colors.white,
+            onPressed: () async {
+              var url = 'tel:${pet.shelter.phone}';
+              if (await canLaunch(url)) {
+                await launch(url);
+              } else {
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      AppLocalizations.of(context).errorUnableToCallShelter,
+                    ),
+                  ),
+                );
+              }
+            },
+          );
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
