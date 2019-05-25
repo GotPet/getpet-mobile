303f0a5b29c83f616f61e94559d9c6b4d7a56e8c
Karolis Vycius <k.vycius@gmail.com>
2019-05-25 12:08:50 +0300

    Execute legacy db migration on separate thread
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- android/app/src/main/kotlin/lt/getpet/getpet/MainActivity.kt
+++ android/app/src/main/kotlin/lt/getpet/getpet/MainActivity.kt
@@ -1,19 +1,16 @@
 package lt.getpet.getpet
 
+
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.room.Room
-
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
-
-
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 import lt.getpet.getpet.persistence.Migrations
 import lt.getpet.getpet.persistence.PetsDatabase
-import lt.getpet.getpet.persistence.PetDao
+import kotlin.concurrent.thread
 
 class MainActivity : FlutterActivity() {
     companion object {
@@ -25,22 +22,30 @@ class MainActivity : FlutterActivity() {
         GeneratedPluginRegistrant.registerWith(this)
 
         MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
+
             if (call.method == "getLegacyPets") {
-                val db = Room.databaseBuilder(
-                        applicationContext,
-                        PetsDatabase::class.java,
-                        "Pets.db"
-                ).addMigrations(Migrations.MIGRATION_3_4)
-                        .allowMainThreadQueries()
-                        .fallbackToDestructiveMigration()
-                        .build()
+                thread {
+                    val db = Room.databaseBuilder(
+                            applicationContext,
+                            PetsDatabase::class.java,
+                            "Pets.db"
+                    ).addMigrations(Migrations.MIGRATION_3_4)
+                            .fallbackToDestructiveMigration()
+                            .build()
 
-                val petsDao = db.petsDao()
+                    val petsDao = db.petsDao()
+                    val linkedPetIds = petsDao.getLikedPetIds()
+                    val petIdsWithRequest = petsDao.getPetIdsWithRequests()
 
-                result.success(mapOf<String, List<Long>>(
-                        "liked_pet_ids" to petsDao.getLikedPetIds(),
-                        "with_requests_pet_ids" to petsDao.getPetIdsWithRequests()
-                ))
+                    runOnUiThread {
+                        result.success(
+                                mapOf(
+                                        "linkedPetIds" to linkedPetIds,
+                                        "petIdsWithRequest" to petIdsWithRequest
+                                )
+                        )
+                    }
+                }
             } else {
                 result.notImplemented()
             }
