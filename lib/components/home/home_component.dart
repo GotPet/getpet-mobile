3ce7f16f7f9a83034702eebb24a8b81d4eeb99eb
Karolis Vycius <k.vycius@gmail.com>
2020-05-17 13:02:18 +0300

    Repleace Sentry by Firebase crashlytics
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- lib/components/home/home_component.dart
+++ lib/components/home/home_component.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:getpet/components/onboarding/onboarding_component.dart';
 import 'package:getpet/components/pet_favorites/favorites_component.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:getpet/components/app/app_component.dart';
 import 'package:getpet/repositories/pets_db_repository.dart';
-import 'package:getpet/utils/crash_reporting.dart';
-import 'package:getpet/utils/debug_utils.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 Future<Null> main() async {
-  final CrashReporting crashReporting = await CrashReporting().init();
+  WidgetsFlutterBinding.ensureInitialized();
 
-  FlutterError.onError = (FlutterErrorDetails details) async {
-    if (isInDebugMode) {
-      FlutterError.dumpErrorToConsole(details);
-    } else {
-      Zone.current.handleUncaughtError(details.exception, details.stack);
-    }
-  };
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runZoned<Future<Null>>(() async {
     await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     await PetsDBRepository().initDb();
 
     runApp(new AppComponent());
-  }, onError: (error, stackTrace) async {
-    await crashReporting.reportError(error, stackTrace);
-  });
+  }, onError: Crashlytics.instance.recordError);
 }
