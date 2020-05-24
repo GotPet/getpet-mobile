b11ea8425efafdb7dd4fc35c848e470a9f00b533
Karolis Vycius <k.vycius@gmail.com>
2020-05-24 18:31:38 +0300

    Pet photo fullscreen
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- lib/components/pet_profile/pet_profile.dart
+++ lib/components/pet_profile/pet_profile.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_networkimage/zoomable.dart';
 import 'package:getpet/authentication/authentication_manager.dart';
 import 'package:getpet/components/shelter_pet/shelter_pet_component.dart';
 import 'package:getpet/components/swipe/pet_card.dart';
@@ -138,31 +139,28 @@ class PetPhotosCarousel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        final screenWidth = getScreenWidth(context);
+    final screenWidth = getScreenWidth(context);
 
-        final photos = pet
-            .allPhotos()
-            .map(
-              (photo) => getSizedImageUrl(
-                photo,
-                screenWidth,
-                ceilToHundreds: true,
-              ),
-            )
-            .mapIndexed(_createImageWidget)
-            .toList(growable: false);
+    final photos = pet
+        .allPhotos()
+        .map(
+          (photo) => getSizedImageUrl(
+            photo,
+            screenWidth,
+            ceilToHundreds: true,
+          ),
+        )
+        .mapIndexed(_createImageWidget)
+        .toList(growable: false);
 
-        return new Carousel(
-          images: photos,
-          dotPosition: DotPosition.topCenter,
-          indicatorBgPadding: 45,
-          overlayShadow: true,
-          dotBgColor: Colors.transparent,
-          autoplay: false,
-        );
-      },
+    return new Carousel(
+      images: photos,
+      dotPosition: DotPosition.topCenter,
+      indicatorBgPadding: 45,
+      overlayShadow: true,
+      dotBgColor: Colors.transparent,
+      autoplay: false,
+      onImageTap: (i) => openPhotoFullscreen(context, pet.allPhotos()[i]),
     );
   }
 
@@ -179,6 +177,17 @@ class PetPhotosCarousel extends StatelessWidget {
     }
 
     return image;
+  }
+
+  Future openPhotoFullscreen(BuildContext context, String url) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (_) => FullScreenImagePage(
+                name: pet.name,
+                url: url,
+              )),
+    );
   }
 }
 
@@ -242,6 +251,38 @@ class DislikePetActionWidget extends StatelessWidget {
             },
           );
         },
+      ),
+    );
+  }
+}
+
+class FullScreenImagePage extends StatelessWidget {
+  final String name;
+  final String url;
+
+  const FullScreenImagePage({
+    Key key,
+    @required this.name,
+    @required this.url,
+  })  : assert(url != null),
+        assert(name != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print(url);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+      ),
+      body: ZoomableWidget(
+        minScale: 1,
+        maxScale: 3,
+        child: Container(
+          child: GetPetNetworkImage(
+            url: url,
+          ),
+        ),
       ),
     );
   }
