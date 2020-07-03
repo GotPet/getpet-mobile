266bed5cbd3a5c465ca38d5a544577519e273943
Karolis Vycius <k.vycius@gmail.com>
2020-07-03 11:12:17 +0300

    Change color of progress_indicator
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- lib/widgets/progress_indicator.dart
+++ lib/widgets/progress_indicator.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 class AppProgressIndicator extends StatelessWidget {
+  final Color color;
+
   const AppProgressIndicator({
     Key key,
+    this.color,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return CircularProgressIndicator(
-      backgroundColor: Theme.of(context).primaryColor,
+    final baseTheme = Theme.of(context);
+
+    return Theme(
+      data: baseTheme.copyWith(
+        accentColor: this.color != null ? this.color : baseTheme.primaryColor,
+      ),
+      child: const CircularProgressIndicator(),
     );
   }
 }
